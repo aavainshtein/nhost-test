ec1a0a1181fc7cfd9bcf4c1497e0b6eb8f11ac2c
Aleksandr Vainshtein <asderq@gmail.com>
2024-08-02 18:53:56 +0300

    init

---
+++ quasar-project/src/boot/nhost.ts
@@ -0,0 +1,14 @@
+import { NhostClient } from "@nhost/vue";
+import { boot } from "quasar/wrappers";
+
+const nhost = new NhostClient({
+  subdomain: "uoijcucqgcnuojadqdrr",
+  region: "eu-central-1",
+});
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$nhost = nhost;
+  app.use(nhost);
+});
+
+// export { nhost };
